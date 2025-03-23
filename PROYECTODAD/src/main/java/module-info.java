ef35cc1b3faa7414a752c028f89a49bc22715eca
eduromero10 <eduromeroafonso04@gmail.com>
2025-03-23 22:48:17 +0000

    Delete PROYECTODAD directory

--- PROYECTODAD/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.proyectodad {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.proyectodad to javafx.fxml;
-    exports com.example.proyectodad;
-}
